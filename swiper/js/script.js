6a91687ad37f69eb038131f7bb560a38817fe108
mengweiyang666 <1181566684@qq.com>
2017-12-05 10:16:17 +0800

    微场景
    1

---
+++ swiper/js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    loop : true,
+    direction: 'vertical',
+    effect : 'flip',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+});
